e43228f553ed509dda9c8790e04516b96eb62baf
John Rademaker <rademakerjohn1@gmail.com>
2020-08-27 22:17:04 -0400

    created form for user initials, created localStorage functionality, added component rendering conditions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useLayoutEffect } from 'react';
+import React, { useState, useLayoutEffect, useEffect } from 'react';
 import axios from 'axios'
 
 function Button({ onClick, difficulty  }) {
@@ -19,21 +19,36 @@ function Answer({ answer, onClick }) {
   )
 }
 
+function Form({ onSubmit, onChange }) {
+  return (
+    <form onSubmit={onSubmit}>
+    <input type="text" onChange={onChange} maxLength="3" />
+    </form>
+  )
+}
+
 function App() {
 
   // State for trivia questions, timer, right/wrong count
+  const [stats, setStats] = useState(JSON.parse(window.localStorage.getItem('stats')) || [])
+  const [initials, setInitials] = useState('')
   const [questions, setQuestions] = useState([])
   const [seconds, setSeconds] = useState();
   const [correct, setCorrect] = useState(0);
+  const [start, setStart] = useState(false)
+  const [end, setEnd] = useState(false)
+  const [error, setError] = useState('')
   const [incorrect, setIncorrect] = useState(0);
 
   // Get token, get questions and set timer and score
   const startQuiz = async (difficulty) => {
+    console.log(stats)
     let token = await axios.get("https://opentdb.com/api_token.php?command=request")
     token = token.data.token;
-    const questions = await getQuestions(token, difficulty);
+    const data = await getQuestions(token, difficulty);
     setSeconds(10)
-    setQuestions(questions)
+    setQuestions(data)
+    setStart(true)
   }
 
   // Get data from API, combine and shuffle right/wrong answers
@@ -45,6 +60,11 @@ function App() {
     })
     return trivia;
   }
+  
+  // When stats state is updated, save to localStorage
+  useEffect(() => {
+    window.localStorage.setItem('stats', JSON.stringify(stats))
+  }, [stats])
 
   // If there are questions in state, setTimeOut decrements timer each second
   useLayoutEffect(() => {
@@ -55,6 +75,14 @@ function App() {
     return () => clearTimeout(timer);
   });
 
+  // If correct or incorrect is non-zero AND no questions left
+  useEffect(() => {
+    if ((correct || incorrect) && !questions.length) {
+      setStart(false)
+      setEnd(true)
+    }
+  }, [correct, incorrect, questions.length])
+
   // If questions left and time reaches 0, increment incorrect count, remove current question, reset timer
   if (questions.length && seconds < 1) {
     setIncorrect(wrong => wrong + 1)
@@ -62,10 +90,6 @@ function App() {
     setSeconds(10);
   }
 
-  // If correct or incorrect is non-zero AND no questions left
-  if ((correct || incorrect) && !questions.length) {
-    console.log("test")
-  }
 
   // If user answer matches questions.correct_answer, increment correct, else increment incorrect
   // Remove current question from array, reset timer
@@ -74,6 +98,25 @@ function App() {
     else setIncorrect(incorrect => incorrect + 1)
     setQuestions(q => q.slice(1));
     setSeconds(10);
+  }
+
+  const handleSave = (event) => {
+    event.preventDefault()
+    if (!isNaN(initials) || !initials) {
+      setError("Please enter at least one initial")
+      return;
+    }
+    let userData = {
+      initials: initials,
+      incorrect: incorrect,
+      correct: correct
+    }
+    setStats(data =>  [...data, userData])  
+    setError("")
+  }
+
+  const handleChange = (event) => {
+    setInitials(event.target.value)
   }
 
   // Randomize array order
@@ -98,9 +141,13 @@ function App() {
 
   return (
     <div>
+      {!start && 
+      <div>
       <Button onClick={() => startQuiz("easy")} difficulty="easy" />
       <Button onClick={() => startQuiz("medium")} difficulty="medium" />
       <Button onClick={() => startQuiz("hard")} difficulty="hard" />
+      </div>
+      }
       {questions.length > 0 &&
         <div>
           <Question question={questions[0].question} />
@@ -112,9 +159,15 @@ function App() {
           ))}
         </div>
       }
-      <p>{seconds}</p>
+      {start && <p>{seconds}</p>}
+      {(start || end) &&
+      <div>
       <p>Correct: {correct}</p>
       <p>Incorrect: {incorrect}</p>
+      </div>
+      }
+      {end && <Form onChange={(event) => handleChange(event)} onSubmit={(event) => handleSave(event)} />}
+      {error && <p>{error}</p>}
     </div>
 
   )
