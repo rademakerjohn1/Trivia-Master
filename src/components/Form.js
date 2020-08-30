702bfe7da25baab05f2e80ead1bafe51419f18ef
John Rademaker <rademakerjohn1@gmail.com>
2020-08-30 19:03:37 -0400

    added submit button and label to form for user initials

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,9 @@ import React from 'react'
 function Form({ onSubmit, onChange, error }) {
     return ( 
         <form onSubmit={onSubmit}>
-            <input type="text" onChange={onChange} maxLength="3" />
+            <label htmlFor="initials">Enter your initials</label><br />
+            <input type="text" name="initials" onChange={onChange} maxLength="3" />
+            <button type="submit">Submit</button>
             {error && <p>{error}</p>}
         </form>
     )
