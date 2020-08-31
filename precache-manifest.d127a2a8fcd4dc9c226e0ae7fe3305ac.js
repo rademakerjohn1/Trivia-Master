d4567190ff839b6e8d4be7e7f7039e680b9a8426
John Rademaker <rademakerjohn1@gmail.com>
2020-08-31 01:01:20 -0400

    Updates

---
+++ precache-manifest.d127a2a8fcd4dc9c226e0ae7fe3305ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa11e601e0408c56109555c055fc8572",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d67a1f01e1e522d5ea8",
+    "url": "/static/css/main.d4a9c279.chunk.css"
+  },
+  {
+    "revision": "39632d58e7d6858c0d23",
+    "url": "/static/js/2.bdd0826a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bdd0826a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d67a1f01e1e522d5ea8",
+    "url": "/static/js/main.3784d572.chunk.js"
+  },
+  {
+    "revision": "9c8877113041a7b74f50",
+    "url": "/static/js/runtime-main.7eab3d4b.js"
+  }
+]);
