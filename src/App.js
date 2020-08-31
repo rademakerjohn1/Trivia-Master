9c2b2ca8688bf2d9b23a30f3790373592259f596
John Rademaker <rademakerjohn1@gmail.com>
2020-08-31 02:06:08 -0400

    adding switch back into routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Header from './components/Header/Header'
 import Quiz from './pages/Quiz'
 import Scoreboard from './pages/Scoreboard'
@@ -9,8 +9,11 @@ function App() {
 
   return (
     <div>
-      <Route exact path="/" component={Quiz} />
-      <Route path="/scores" component={Scoreboard} />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Quiz} />
+        <Route exact path="/scores" component={Scoreboard} />
+      </Switch>
     </div>
   )
 }
