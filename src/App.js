9e735e0595be19af4b9890fe49c652fe0221e35f
John Rademaker <rademakerjohn1@gmail.com>
2020-08-31 01:23:34 -0400

    trying HashRouter for Github deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Link } from "react-router-dom";
 import Header from './components/Header/Header'
 import Quiz from './pages/Quiz'
 import Scoreboard from './pages/Scoreboard'
@@ -8,13 +8,10 @@ import Scoreboard from './pages/Scoreboard'
 function App() {
 
   return (
-      <Router>
-        <Header />
-        <Switch>
+      <HashRouter basename="/">
           <Route exact path="/" component={Quiz} />
-          <Route exact path="/scores" component={Scoreboard} />
-        </Switch>
-      </Router>
+          <Route path="/scores" component={Scoreboard} />
+      </HashRouter>
   )
 }
 
