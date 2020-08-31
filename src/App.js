645c7d8ad5b6efe8cda3cdc2a343c9339a8141d3
John Rademaker <rademakerjohn1@gmail.com>
2020-08-31 01:53:04 -0400

    moved hash router to index.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from './components/Header/Header'
 import Quiz from './pages/Quiz'
 import Scoreboard from './pages/Scoreboard'
@@ -8,11 +8,13 @@ import Scoreboard from './pages/Scoreboard'
 function App() {
 
   return (
-      <HashRouter basename="/">
-          <Header />
-          <Route exact path="" component={Quiz} />
-          <Route path="/scores" component={Scoreboard} />
-      </HashRouter>
+    <Router>
+    <Header />
+    <Switch>
+      <Route exact path="/" component={Quiz} />
+      <Route exact path="/scores" component={Scoreboard} />
+    </Switch>
+  </Router>
   )
 }
 
