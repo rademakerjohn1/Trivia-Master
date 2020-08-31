02ed2c43913bc06a3089eb0c2f4d9a7ad6829fc3
John Rademaker <rademakerjohn1@gmail.com>
2020-08-30 20:26:01 -0400

    added header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from './components/Header'
 import Quiz from './pages/Quiz'
 import Scoreboard from './pages/Scoreboard'
 
@@ -8,6 +9,7 @@ function App() {
 
   return (
       <Router>
+        <Header />
         <Switch>
           <Route exact path="/" component={Quiz} />
           <Route exact path="/scores" component={Scoreboard} />
