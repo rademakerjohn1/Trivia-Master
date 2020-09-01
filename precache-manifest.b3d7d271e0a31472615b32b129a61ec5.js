bddb16ea1aa3e2eaa0bc1d1ae530cd89b26d36b6
John Rademaker <rademakerjohn1@gmail.com>
2020-09-01 16:38:00 -0400

    Updates

---
+++ precache-manifest.b3d7d271e0a31472615b32b129a61ec5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "802f3e6cf01d56410104185d67a9f89d",
+    "url": "/trivia-master/index.html"
+  },
+  {
+    "revision": "f5c3fcea814d9a74d214",
+    "url": "/trivia-master/static/css/main.627cdcfd.chunk.css"
+  },
+  {
+    "revision": "6300c93f3efec6ca584c",
+    "url": "/trivia-master/static/js/2.8b9e097b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trivia-master/static/js/2.8b9e097b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5c3fcea814d9a74d214",
+    "url": "/trivia-master/static/js/main.cd5363a6.chunk.js"
+  },
+  {
+    "revision": "8647eacd3b8c2ba98d36",
+    "url": "/trivia-master/static/js/runtime-main.19cda06b.js"
+  },
+  {
+    "revision": "0917fd95ea919d70f1d7cd1e664a1830",
+    "url": "/trivia-master/static/media/pattern299-01.0917fd95.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Trivia-Master/precache-manifest.ccbbde50cf008ec723032d78b3beab6a.js"
+  "/trivia-master/precache-manifest.b3d7d271e0a31472615b32b129a61ec5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Trivia-Master/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/trivia-master/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
