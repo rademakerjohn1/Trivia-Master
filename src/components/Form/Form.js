75265a6826218f61a4ad260ee4aa66a88eb1fd16
John Rademaker <rademakerjohn1@gmail.com>
2020-09-07 17:17:22 -0400

    add link to start over at quiz end

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Form.css'
+import PageLink from '../PageLink/PageLink'
 
 function Form({ end, onSubmit, onChange, error }) {
     return (
@@ -9,6 +10,7 @@ function Form({ end, onSubmit, onChange, error }) {
             <input type="text" name="initials" onChange={onChange} maxLength="3" />
             <button type="submit">Submit</button>
             {error && <p id="error">{error}</p>}
+            <PageLink destination="#/" message={"Play Again"} />
         </form>
     )
 }
