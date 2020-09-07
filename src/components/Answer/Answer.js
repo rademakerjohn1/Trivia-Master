c138eaeb273abf3349d39565b2334f14752a4893
John Rademaker <rademakerjohn1@gmail.com>
2020-09-07 15:40:01 -0400

    disable answer click events on render of answer feedback

--- src/components/Answer/Answer.js
+++ src/components/Answer/Answer.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import './Answer.css'
 
-function Answer({ answer, onClick }) {
-    return <li className="answer" onClick={onClick}>{answer}</li>
+function Answer({ answer, onClick, message }) {
+    return <li className="answer" onClick={!message ? onClick : null}>{answer}</li>
   }
 
 export default Answer;
